c4badc610796bbf4ab6b7966de3e0ac9d029e2b8
Ritika <143311430+Ritika1529@users.noreply.github.com>
2024-06-23 18:16:13 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,39 @@
+// Hide the cross icon initially
+
+// const hamBrger=document.querySelector(".cross");
+// const hBurger=document.querySelector(".bar");
+
+//   const myFunction = function myFunction(){
+//     var element = document.querySelector(".bar");
+//     element.classList.toggle("hamBurger");
+//     console.log("running")
+//     }
+//     myFunction();
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   const hBurger = document.querySelector(".bar");
+
+//    function myFunction(){
+//       hBurger.classList.toggle("hamBurger");
+//       console.log("running");
+//   }
+
+//   hBurger.addEventListener("click", myFunction);
+// })
+
+
+
+
+// document.querySelector('.cross').computedStyleMap.display = 'none';
+// document.querySelector('.bar').addEventListener("click" ,() =>{
+//   document.querySelector('.sideBar').classList.toggle('sidebarGo');
+//   console.log("yyy")
+// })
+
+
+
